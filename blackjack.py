7d993895db2c2fb2f0b2fc56fbe4a0c774e8210a
drknsblct <drknsblct@gmail.com>
2021-05-14 16:45:06 +0300

    removed list and added variables wins/losses

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,8 @@
 from deck import Deck
 from player import Player
 
-games = []
+wins = 0
+losses = 0
 
 
 class Blackjack:
@@ -15,19 +16,22 @@ class Blackjack:
         # create dealer
         self.dealer = Player(True, self.deck)
 
+    # wins = 0
+    # losses = 0
+
     def play(self, num):
-        global games
+        global wins, losses
         player_status = self.player.deal()
         dealer_status = self.dealer.deal()
 
         if player_status == 1:
             # if player draws 21 first try
-            games.append('Wins')
+            wins += 1
 
             if dealer_status == 1:
                 # if dealer draws 21 first try
-                games.append('Losses')
-            return 1
+                losses += 1
+            return
 
         while self.player.score <= num:
             # if player's score is below num then we add
@@ -37,24 +41,26 @@ class Blackjack:
                 bust = self.player.hit()
             if bust == 1:
                 # if player has cards with values > 21
-                games.append('Losses')
-                return 1
+                losses += 1
+                return
 
         if dealer_status == 1:
             # if dealer's cards have value == 21
-            games.append('Losses')
-            return 1
+            losses += 1
+            return
 
         while self.dealer.check_score() <= 17:
             # dealer must have at least 17 card value according to the rules
             if self.dealer.hit() == 1:
                 # if dealer's cards have value > 21 then we win
-                games.append('Wins')
-                return 1
+                wins += 1
+                return
 
         if self.dealer.check_score() == self.player.check_score():
-            games.append('Losses')
+            losses += 1
+
         elif self.dealer.check_score() > self.player.check_score():
-            games.append('Losses')
+            losses += 1
+
         elif self.dealer.check_score() < self.player.check_score():
-            games.append('Wins')
+            wins += 1
--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from collections import Counter
-from blackjack import Blackjack, games
-from logo import logo
 import re
+import blackjack
+from blackjack import Blackjack
+from logo import logo
 
 
 def print_wins_and_losses(max_number):
     for _ in range(num_loops):
         Blackjack().play(max_number)
 
-    counts = Counter(games)
-    wins = counts.get('Wins')
-    losses = counts.get('Losses')
+    print(f'Wins: {blackjack.wins} | Losses: {blackjack.losses}')
+    print(f'Wins: {blackjack.wins / num_loops * 100:.2f}% | Losses: {blackjack.losses / num_loops * 100:.2f}%')
 
-    if wins is None:
-        wins = 0
-    elif losses is None:
-        losses = 0
-
-    print(f'Wins: {wins} | Losses: {losses}')
-    print(f'Wins: {wins / num_loops * 100:.2f}% | Losses: {losses / num_loops * 100:.2f}%')
-
-    # clear games list to run again with new card stand
-    games.clear()
     print('\n\n')
 
 
